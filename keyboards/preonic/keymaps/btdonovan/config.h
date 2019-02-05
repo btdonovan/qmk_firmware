fbaffc81b640bf8aa05d2fb10a65a6d7c859cae2
Benjamin Donovan <carcel@navonod.us>
2019-02-04 22:58:50 -0700

    Finalized initial keymap, but considering moving enter to left of space key.

--- keyboards/preonic/keymaps/btdonovan/config.h
+++ keyboards/preonic/keymaps/btdonovan/config.h
@@ -3,6 +3,10 @@
 
 #include "config_common.h"
 
+#define LEADER_PER_KEY_TIMING
+
+#define LEADER_TIMEOUT 250
+
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
--- keyboards/preonic/keymaps/btdonovan/keymap.c
+++ keyboards/preonic/keymaps/btdonovan/keymap.c
@@ -23,7 +23,7 @@ enum preonic_layers {
   _DVORAK,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
   _NUM
 };
 
@@ -33,7 +33,6 @@ enum preonic_keycodes {
   DVORAK,
   LOWER,
   RAISE
-  }
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -105,20 +104,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |      |      |   -  |   =  |   [  |   ]  |  |   |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |      |   _  |   =  |   [  |   ]  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  F5  |  F6  |  F7  |  F8  |      |      |   _  |   +  |   {  |   }  |  "   |
+ * | Ctrl |  F5  |  F6  |  F7  |  F8  |      |      |   -  |   +  |   {  |   }  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F9  |  F10 |  F11 |  F12 |      |      |ISO ~ |ISO | |ISO # |ISO / | Ent  |
+ * |      |  F9  |  F10 |  F11 |  F12 |      |      |      |      |  <   |  >   | Ent  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR,    KC_LPRN,  KC_RPRN, KC_BSPC, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_MINS,   KC_EQL,     KC_LBRC,  KC_RBRC, KC_PIPE,  \
-  KC_LCTL, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_UNDS,   KC_PLUS,    KC_LCBR,  KC_RCBR, KC_DQT, \
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, S(KC_NUHS),S(KC_NUBS), KC_NUHS,  KC_NUBS,  KC_ENT, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_UNDS,   KC_EQL,     KC_LBRC,  KC_RBRC, KC_PIPE,  \
+  KC_LCTL, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_MINS,   KC_PLUS,    KC_LCBR,  KC_RCBR, KC_DQT, \
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,   _______,    KC_LT,    KC_GT,  KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______, _______,   KC_HOME,    KC_PGDN,  KC_PGUP, KC_END \
 ),
 
@@ -162,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_CAPS, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
-)
+),
 
 /* Num (NUM Toggle)
  * ,-------------------------------------------------------------------------------------.
@@ -178,7 +177,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------'
  */
 [_NUM] = LAYOUT_preonic_grid( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_PERC, KC_PSLS, KC_PAST, KC_DEL, \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_NLCK, KC_PSLS, KC_PAST, KC_DEL, \
   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_CALC, KC_LPRN, KC_RPRN, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, \
   KC_LCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F,    KC_LBRC, KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
   _______, KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_LCBR, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   RSFT_T(KC_PENT), \

285a9abc42cdea94442ee7106915287422414e61
Benjamin Donovan <carcel@navonod.us>
2019-02-22 19:14:11 -0700

    Accidentally left keycodes configured for lower left keys on querty

--- keyboards/preonic/keymaps/btdonovan/keymap.c
+++ keyboards/preonic/keymaps/btdonovan/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
  * ║ LSft │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │RShEnt║
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
- * ║ Num  │Leader│ GUI  │ Alt  │Lower │ Ent  │Space │Raise │ Left │ Down │  Up  │Right ║
+ * ║      │      │ GUI  │ Alt  │Lower │ Ent  │Space │Raise │ Left │ Down │  Up  │Right ║
  * ╚══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╝
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLASH,  \
   LCTL_T(KC_ESC), KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
   KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),  \
-  TG(_NUM),       KC_LEAD,  KC_LGUI,  KC_LALT,  LOWER,    KC_ENT,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT  \
+  KC_NO,          KC_NO,    KC_LGUI,  KC_LALT,  LOWER,    KC_ENT,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT  \
 ),
 
 /* Colemak

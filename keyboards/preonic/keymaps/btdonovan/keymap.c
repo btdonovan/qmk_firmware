7052c6fbd556ea1557a9021e3961586a5c51c487
Benjamin Donovan <carcel@navonod.us>
2019-02-22 21:41:25 -0700

    Just on querty layout for now.

--- keyboards/preonic/keymaps/btdonovan/keymap.c
+++ keyboards/preonic/keymaps/btdonovan/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
  * ║ LSft │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │RShEnt║
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
- * ║      │      │ GUI  │ Alt  │Lower │ Ent  │Space │Raise │ Left │ Down │  Up  │Right ║
+ * ║      │      │ GUI  │ Alt  │Lower │LshEnt│Space │Raise │ Left │ Down │  Up  │Right ║
  * ╚══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╝
  */
 [_QWERTY] = LAYOUT_preonic_grid( \

bed192dcce41faa17de8985eb5ffb27cecbcbe90
Benjamin Donovan <carcel@navonod.us>
2019-03-16 09:49:01 -0600

    Created game layer.

--- keyboards/preonic/keymaps/btdonovan/keymap.c
+++ keyboards/preonic/keymaps/btdonovan/keymap.c
@@ -22,6 +22,7 @@ enum preonic_layers {
   _COLEMAK,
   _DVORAK,
   _NUM,
+  _GAME,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -32,6 +33,7 @@ enum preonic_keycodes {
   COLEMAK,
   DVORAK,
   NUM,
+  GAME,
   LOWER,
   RAISE
 };
@@ -122,6 +124,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,          KC_NO,    KC_LGUI,  KC_LALT,  LOWER,    LSFT_T(KC_ENT), KC_SPC,   RAISE,    KC_P0,    KC_DOT,   KC_COMM,  KC_DEL  \
 ),
 
+/* Game
+ * ╔══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╗
+ * ║ Esc  │   1  │   2  │   3  │   4  │   5  │   6  │   7  │   8  │   9  │   0  │ Bksp ║
+ * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
+ * ║ Tab  │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │  \   ║
+ * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
+ * ║CtrEsc│   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │  "   ║
+ * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
+ * ║ LSft │   Z  │   X  │   C  │   V  │   B  │   N  │   M  │   ,  │   .  │   /  │RShEnt║
+ * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
+ * ║      │      │      │ Alt  │Lower │Space │RshEnt│Raise │ Left │ Down │  Up  │Right ║
+ * ╚══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╝
+ */
+[_GAME] = LAYOUT_preonic_grid( \
+  KC_ESC,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,             KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC, \
+  KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,             KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLASH,  \
+  LCTL_T(KC_ESC), KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,             KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+  KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,             KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),  \
+  KC_NO,          KC_NO,    KC_NO,    KC_LALT,  LOWER,    KC_SPC,   RSFT_T(KC_ENT),   RAISE,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT  \
+),
+
 /* Lower
  * ╔══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╤══════╗
  * ║   ~  │   !  │   @  │   #  │   $  │   %  │   ^  │   &  │   *  │   (  │   )  │ Del  ║
@@ -172,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
  * ║CapsLk│      │MuScl │Aud on│AudOff│AGnorm│AGswap│Qwerty│Colemk│Dvorak│      │      ║
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
- * ║      │Voice-│Voice+│Mus on│MusOff│MidiOn│MidOff│ Num  │      │      │      │      ║
+ * ║      │Voice-│Voice+│Mus on│MusOff│MidiOn│MidOff│ Num  │ Game │      │      │      ║
  * ╟──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────╢
  * ║      │      │      │      │      │      │      │      │      │      │      │      ║
  * ╚══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╧══════╝
@@ -181,7 +204,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  \
   _______,  RESET,    DEBUG,    _______,  _______,  _______,  _______,  TERM_ON,  TERM_OFF, _______,  _______,  KC_DEL,  \
   KC_CAPS,  _______,  MU_MOD,   AU_ON,    AU_OFF,   AG_NORM,  AG_SWAP,  QWERTY,   COLEMAK,  DVORAK,   _______,  _______, \
-  _______,  MUV_DE,   MUV_IN,   MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   NUM,      _______,  _______,  _______,  _______, \
+  _______,  MUV_DE,   MUV_IN,   MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   NUM,      GAME,     _______,  _______,  _______, \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______  \
 )
 
@@ -210,6 +233,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case NUM:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_NUM);
+          }
+          return false;
+          break;
+        case GAME:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_GAME);
           }
           return false;
           break;
